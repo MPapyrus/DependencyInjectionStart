cb52d0a0319caf77a9b6dac8bfd5b1b1b32dd77d
MPapyrus <secenmasterdead@yandex.ru>
2025-03-19 09:45:59 +0500

    Use provides methods instead inject

--- app/src/main/java/com/example/dependencyinjectionstart/example1/NewComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/NewComponent.kt
@@ -5,9 +5,5 @@ import dagger.Component
 @Component(modules = [ComputerModule::class])
 interface NewComponent {
 
-    fun getKeyboard(): Keyboard
-
-    fun getMouse(): Mouse
-
     fun inject(activity: Activity)
 }
