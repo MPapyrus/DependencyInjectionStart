da38231717aff9a8feaa3ed2867173152d23d409
MPapyrus <secenmasterdead@yandex.ru>
2025-03-19 08:51:21 +0500

    Added Dagger and used his annotations

---
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/NewComponent.kt
@@ -0,0 +1,11 @@
+package com.example.dependencyinjectionstart.example1
+
+import dagger.Component
+
+@Component
+interface NewComponent {
+
+    fun getKeyboard(): Keyboard
+
+//    fun inject(activity: Activity)
+}
