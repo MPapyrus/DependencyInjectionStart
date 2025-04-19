ae32d90941948978421f068e796d55dd34d92703
MPapyrus <secenmasterdead@yandex.ru>
2025-04-20 00:04:26 +0500

    Created custom builder instead of Modules with parameters

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -1,17 +1,32 @@
 package com.example.dependencyinjectionstart.example2.di
 
+import android.content.Context
 import com.example.dependencyinjectionstart.example2.presentation.MainActivity
+import dagger.BindsInstance
 import dagger.Component
 
 @Component(
     modules = [
         DomainModule::class,
         DataModule::class,
-        ContextModule::class
     ]
 )
 interface ApplicationComponent {
 
     fun inject(activity: MainActivity)
 
+
+    @Component.Builder
+    interface ApplicationComponentBuilder {
+
+
+        @BindsInstance
+        fun context(context: Context): ApplicationComponentBuilder
+
+        @BindsInstance
+        fun currentTime(timeMillis: Long): ApplicationComponentBuilder
+
+        fun build(): ApplicationComponent
+
+    }
 }
